984eee813d4e57ae2c02e6927de7377b79e5a661
Julio Acevedo Jimenez <multiappsas@gmail.com>
2024-12-04 16:55:48 -0500

    Menu desplegable version 00.1

---
+++ menu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <nav>
+            <ul class="menu-horizontal">
+                <li><a href="administrador.php">Inicio</a></li>
+                <li>
+                    <a href="#">Vehículo</a>
+                    <ul class="menu-vertical">
+                        <li><a href="vehiculo.php">Nuevo Vehículo</a></li>
+                        <li><a href="">Actualizar</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Proveedor</a>
+                    <ul class="menu-vertical">
+                        <li><a href="proveedor.php">Nuevo Proveedor</a></li>
+                        <li><a href="#">Actualizar Proveedor</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Funcionario</a>
+                    <ul class="menu-vertical">
+                        <li><a href="usuario.php">Nuevo Funcionario</a></li>
+                        <li><a href="#">Actualizar Funcionario</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="#">Conductor</a>
+                    <ul class="menu-vertical">
+                        <li><a href="#">Nuevo Conductor</a></li>
+                        <li><a href="#">Actualizar Conductor</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+</body>
+</html>
